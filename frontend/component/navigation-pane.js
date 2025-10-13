910b0fbb834affaeb7630f048c88720a054cf2ea
nazaratius <53535394+nazaratius@users.noreply.github.com>
2025-10-13 09:24:37 +0200

    made some progress setting up the UI

--- frontend/component/navigation-pane.js
+++ frontend/component/navigation-pane.js
@@ -1,14 +1,33 @@
 class NavigationPane {
   #mainPaneRef;
+  #loadedFileListDOM;
   #files = [];
   constructor(mainPane) {
     this.#mainPaneRef = mainPane;
+    this.#loadedFileListDOM = document.getElementById('loaded-file-list');
 
   }
 
   loadAndSelectFile(file) {
     this.#files.push(file);
     this.#mainPaneRef.display(file);
+
+    const loadedFileEntry = document.createElement('button');
+    loadedFileEntry.dataset.filename = file.name;
+    loadedFileEntry.className = 'loaded-file';
+    loadedFileEntry.textContent = file.name;
+    loadedFileEntry.addEventListener('click', () => { this.#mainPaneRef.display(file); })
+
+    console.log(`#${file.name}`);
+    const existingFileEntry = this.#loadedFileListDOM.querySelector(`[data-filename='${file.name}']`);
+    console.log(existingFileEntry);
+
+    if (existingFileEntry != null) {
+      window.alert('found duplicate entry, replacing existing entry with new one');
+      this.#loadedFileListDOM.replaceChild(loadedFileEntry, existingFileEntry);
+    } else {
+      this.#loadedFileListDOM.appendChild(loadedFileEntry);
+    }
   }
 }
 
--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,11 +1,12 @@
-import {NavigationPane} from "./component/navigation-pane.js";
-import { MainPane } from "./component/main-pane.js";
-import { configureHtmlJsInterface } from "./component/html-adapter.js";
+import {NavigationPane} from './component/navigation-pane.js';
+import { MainPane } from './component/main-pane.js';
+import { configureHtmlJsInterface } from './component/html-adapter.js';
 
 const mainPane = new MainPane(
   {
-    textArea: document.getElementById("editing-textarea"),
-    urlInput: document.getElementById("urlInput") 
+    textArea: document.getElementById('editing-textarea'),
+    urlInput: document.getElementById('url-input'),
+    structurePane: document.getElementById('structure-pane')
   });
 
 const navigationPane = new NavigationPane(mainPane);
